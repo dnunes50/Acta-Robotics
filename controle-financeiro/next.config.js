cff16860b865b1ed4a444cad6b13ac75e1979bf7
dnunes50 <diegoonunes50@gmail.com>
2026-06-18 17:25:29 -0300

    atualizar

--- controle-financeiro/next.config.js
+++ controle-financeiro/next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.externals = [...(config.externals || []), { canvas: 'canvas' }]
-    return config
-  },
+  turbopack: {},
 }
 module.exports = nextConfig
