9c02cf8c7882d62f5602b7900bddf0401225b0cb
dnunes <diegoonunes50@gmail.com>
2026-06-18 16:46:59 -0300

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.externals = [...(config.externals || []), { canvas: 'canvas' }]
-    return config
-  },
-}
-module.exports = nextConfig
